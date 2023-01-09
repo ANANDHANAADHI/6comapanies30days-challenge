81d5520af0bd0ce5c822ed51eeb34d81f2b26a5c
Anandhan <anandhangithub@gmail.com>
2023-01-09 21:49:29 +0530

    algorithm implementation

--- sieve-of-eratosthenes.py
+++ sieve-of-eratosthenes.py
@@ -1,1 +1,8 @@
-c
+n = int(input())
+f = [1] * n
+for i in range(2,int(n**0.5)+1):
+    if (f[i-1]):
+        for j in range(i+i,n+1,i):
+            f[j-1] = 0
+a= [i for i in range(2,n+1) if f[i-1] ==1 ]
+print(a)
