003e756de494eca67812e6b5573f809267aa1545
Anandhan <anandhangithub@gmail.com>
2023-01-09 12:46:50 +0530

    Update 172. Factorial Trailing Zeroes.py

--- 172. Factorial Trailing Zeroes.py
+++ 172. Factorial Trailing Zeroes.py
@@ -5,13 +5,15 @@ class Solution:
         else:
             return n*self.fact(n-1)
     def trailingZeroes(self, n: int) -> int:
-        s = self.fact(n)
-        ans = 0
-        while(True):
-            if s%10 == 0:
-                ans+=1
-                s = s//10
-            else:
-                break
-
-        return ans
+    #    print(self.fact(n))
+       ans = n//5
+       a = ans
+       while(True):
+        ans+= a//5
+        if a//5 <5:
+            break
+        a = a//5
+       return ans
+s = Solution()
+n =int(input())
+print(s.trailingZeroes(n))
